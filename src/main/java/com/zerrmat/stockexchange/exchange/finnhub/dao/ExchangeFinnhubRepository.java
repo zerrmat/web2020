24274b1775bba256bcfcb9aa4a175f599ce43c80
Lukasz Wollnikowski <zerr@mat.umk.pl>
2020-08-03 15:27:22 +0200

    Refactor phase 1, got rid of execution errors, database maintaining pulled up from database to server

--- src/main/java/com/zerrmat/stockexchange/exchange/finnhub/dao/ExchangeFinnhubRepository.java
+++
@@ -1,7 +0,0 @@
-package com.zerrmat.stockexchange.exchange.finnhub.dao;
-
-import com.zerrmat.stockexchange.exchange.model.ExchangeModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ExchangeFinnhubRepository extends JpaRepository<ExchangeModel, Long> {
-}
